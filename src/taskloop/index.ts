2111b5a2937cdd803aaf89b943dad93f66cc73a9
Ro Ngoc Vo <ngocro208@gmail.com>
2025-04-10 16:13:03 +0700

    refactor: remove taskloop since our model cannot handle structure output

--- src/taskloop/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./taskloop.types";
-export * from "./taskloop";
--- src/taskloop/taskloop.types.ts
+++
@@ -1,4 +0,0 @@
-import type { AgentConfig, AgentRunConfig } from "agents";
-import type { z } from "zod";
-
-export type TaskLoopParams<TResponseFormat = "text" | z.ZodType<any>> = AgentConfig & AgentRunConfig<TResponseFormat>;
